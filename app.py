4fea8f7a538f5cd53e588e654a210919bec9e7a7
Fate <rahulsaggu47@gmail.com>
2026-03-28 03:56:08 +0530

    peak added

--- app.py
+++ app.py
@@ -223,6 +223,7 @@ SPOTLIGHT_VIDEO_MAP = {
     403: "/static/videos/dhurandhar.mp4",
     234: "/static/videos/your_namr.mp4",
     265: "/static/videos/jjk_s2.mp4",
+    292: "/static/videos/jjk_s3.mp4",
     402: "/static/videos/3_idiots.mp4",
     254: "/static/videos/love_is_war_movie.mp4",
     469: "/static/videos/tangled.mp4",
